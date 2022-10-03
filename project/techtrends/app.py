8879c75ae90c709e06ee4adbf2a3e11dff9099c1
drgs <dragos.serban@philips.com>
2022-10-03 20:03:13 +0200

    Add dockerfile, healthz and metrics endpoints

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,12 +1,18 @@
 import sqlite3
+import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
+    db_connection_count += 1
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -17,6 +23,15 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
+
+
+# Function to get the number of posts available in the database
+def get_post_count():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) from posts').fetchone()
+    connection.close()
+    return post_count[0]
+
 
 # Define the Flask application
 app = Flask(__name__)
@@ -36,13 +51,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.debug(f"Could not find post with ID {post_id} - does not exist")
       return render_template('404.html'), 404
     else:
+      app.logger.debug(f"Retrieved article with title {post[2]}")
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.debug("Retrieved About Us page")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +78,35 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.debug(f"Created article with title {title}")
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"db_connection_count": db_connection_count, "post_count": get_post_count()}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging_format = "%(levelname)s:%(name)s:%(asctime)s:%(message)s"
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format=logging_format)
+    app.run(host='0.0.0.0', port='3111')
